33e9731d579e262899ddfbe44fb28c7189e795d7
Albert Gasset <albertgasset@fsfe.org>
2017-11-26 18:10:11 +0100

    Translation update (ca)

--- lang/ca/settings.php
+++ lang/ca/settings.php
@@ -1,4 +1,11 @@
 <?php
 
-$lang['nativeeditor'] = 'Utilitza l\'editor natiu de DokuWiki per defecte';
-$lang['codesyntax'] = 'Utilitza el CodeMirror per ressaltar la sintaxi dels blocs de codi en visualitzar les pàgines.';
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Albert Gasset <albertgasset@fsfe.org>
+ */
+$lang['nativeeditor']          = 'Utilitza l\'editor natiu de DokuWiki per defecte.';
+$lang['codesyntax']            = 'Utilitza el CodeMirror per ressaltar la sintaxi dels blocs de codi en visualitzar les pàgines.';
+$lang['usenativescroll']       = 'Utilitza barres de desplaçament natives en lloc de sobreposades.';
+$lang['autoheight']            = 'Alçada automàtica.';
