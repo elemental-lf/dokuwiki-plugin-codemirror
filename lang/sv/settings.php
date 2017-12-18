d89e236a22e1c46373bebe8de652679603137ccf
Tor Härnqvist <tor@harnqvist.se>
2017-12-18 22:55:15 +0100

    translation update (sv)

---
+++ lang/sv/settings.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Tor Härnqvist <tor@harnqvist.se>
+ */
+$lang['nativeeditor']          = 'Använd ordinarie DokuWiki-redigerare som standard.';
+$lang['codesyntax']            = 'Använd CodeMirror för att framhäva syntax i kodblock när sidor visas.';
+$lang['autoheight']            = 'Automatisk höjd.';
