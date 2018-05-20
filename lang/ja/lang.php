3e7d708bd3770a48e960c333bfa957bfb0023aec
HokkaidoPerson <dosankomali@yahoo.co.jp>
2018-05-20 07:40:35 +0200

    translation update (jp)

--- lang/ja/lang.php
+++ lang/ja/lang.php
@@ -1,14 +1,17 @@
 <?php
- 
+
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author HokkaidoPerson <dosankomali@yahoo.co.jp>
  */
 $lang['js']['setting_activeline'] = '現在行の強調表示';
 $lang['js']['setting_closebrackets'] = '閉じ括弧の自動入力';
 $lang['js']['setting_fontsize'] = 'フォントサイズ';
-$lang['js']['setting_keymap'] = 'キーボードマッピング';
+$lang['js']['setting_keymap']  = 'キーボードマッピング';
 $lang['js']['setting_linenumbers'] = '行番号表示';
 $lang['js']['setting_matchbrackets'] = '対応する括弧の強調表示';
-$lang['js']['setting_syntax'] = '構文の強調表示';
-$lang['js']['setting_theme'] = 'カラーテーマ';
+$lang['js']['setting_nativeeditor'] = '元々のDokuWikiエディター';
+$lang['js']['setting_syntax']  = '構文の強調表示';
+$lang['js']['setting_theme']   = 'カラーテーマ';
+$lang['js']['setting_showinvisibles'] = 'ホワイトスペースを表示する';
--- lang/ja/settings.php
+++ lang/ja/settings.php
@@ -3,8 +3,10 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author HokkaidoPerson <dosankomali@yahoo.co.jp>
  * @author Hideaki SAWADA <chuno@live.jp>
  */
 $lang['nativeeditor']          = 'デフォルトで Dokuwiki 編集画面を使用する。';
 $lang['codesyntax']            = 'ページを表示する際、コードブロックの構文をの強調表示用に CodeMirror を使用する。';
 $lang['usenativescroll']       = 'デフォルトの置換え形式の代わりに本来のスクロールバー形式を使用';
+$lang['autoheight']            = '高さの自動調節';
