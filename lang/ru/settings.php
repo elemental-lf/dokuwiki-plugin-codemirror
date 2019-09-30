fe3ddc6a59d50cdad4b75d1c6e89142f7431e780
xambey <xambey@yandex.ru>
2019-09-30 15:15:10 +0200

    translation update

--- lang/ru/settings.php
+++ lang/ru/settings.php
@@ -2,7 +2,11 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author xambey <xambey@yandex.ru>
  * @author Aleksandr Selivanov <alexgearbox@yandex.ru>
  */
 $lang['nativeeditor']          = 'Использовать родной редактор «Докувики» по умолчанию';
+$lang['codesyntax']            = 'Используйте CodeMirror, чтобы выделить синтаксис блоков кода при просмотре страниц. ';
+$lang['usenativescroll']       = 'Используйте собственный стиль полосы прокрутки вместо стиля наложения по умолчанию.';
+$lang['autoheight']            = 'Автоматическая высота.';
