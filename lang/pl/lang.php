d824be3a7445f5cd6ff828c66bea9bad002d0531
Bartek S <sadupl@gmail.com>
2019-10-08 20:15:25 +0200

    translation update

---
+++ lang/pl/lang.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Bartek S <sadupl@gmail.com>
+ */
+$lang['js']['setting_activeline'] = 'Podświetl bieżącą linię';
+$lang['js']['setting_closebrackets'] = 'Automatyczne zamykanie nawiasów';
+$lang['js']['setting_fontsize'] = 'Rozmiar czcionki';
+$lang['js']['setting_linenumbers'] = 'Wyświetl numery linii';
+$lang['js']['setting_matchbrackets'] = 'Podświetl pasujące nawiasy';
+$lang['js']['setting_nativeeditor'] = 'Natywny edytor DokuWiki';
+$lang['js']['setting_syntax']  = 'Podświetl składnię';
+$lang['js']['setting_theme']   = 'Motyw kolorystyczny';
+$lang['js']['setting_showinvisibles'] = 'Pokaż białe znaki';
---
+++ lang/pl/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Bartek S <sadupl@gmail.com>
+ */
+$lang['nativeeditor']          = 'Użyj domyślnego edytora DokuWiki.';
+$lang['autoheight']            = 'Automatyczna wysokość.';
